255b379d64f3d300d55aaace4288c26868645dae
gautham srinivasan <gauthamsrinivas10@gmail.com>
2025-07-07 01:46:41 -0700

    Story changes

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate  } from 'react-router-dom';
 
 function Stories() {
+
+  const [Stories , setStories] = useState([]);
+
+  const navigate = useNavigate();
+
+  let tot = 0;
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data => data.json())
+    .then(data => setStories(data))
+    .catch(err => console.log(err))
+  },[]);
   return (
-    <div className='bg-info story' >Stories</div>
+    <div className='story d-flex' >
+      <div className='d-none'>{tot = Stories.length}</div>
+      
+      {Stories.length > 0 ? 
+      (Stories.map((story)=>(
+        <div key={story.id} className='mx-1' onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
+          <div className='gradient-border'>
+            <img src={story.user.profile_pic} alt="dp" className='story-dp rounded-circle d-flex'/>
+          </div>
+          <p className='text-truncate' style={{width: "50px"}}>{story.user.username}</p>
+        </div>
+      )) 
+    )  : (
+    <p>Loading</p>
+    )}
+    </div>
   )
 }
 
